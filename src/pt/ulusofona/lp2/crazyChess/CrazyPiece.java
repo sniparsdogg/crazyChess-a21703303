43e5913ac071cdc33ac2700ed6344f903ee56d79
sniparsdogg <estacontapertence@gmail.com>
2018-11-27 16:39:35 +0000

    Foram improvadas outras funções.

---
+++ src/pt/ulusofona/lp2/crazyChess/CrazyPiece.java
@@ -0,0 +1,51 @@
+package pt.ulusofona.lp2.crazyChess;
+
+public class CrazyPiece {
+    public class Posicao {
+        int x;
+        int y;
+        Posicao() {
+
+        }
+    }
+
+    int id;
+    int idTipo;
+    int idEquipa;
+    String alcunha;
+    Posicao posicao;
+
+
+    CrazyPiece(){
+        posicao = new Posicao();
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public void setId(int i){
+        this.id = i;
+    }
+
+    public void setIdTipo(int i) {
+        this.idTipo = i;
+    }
+
+    public void setIdEquipa(int i) {
+        this.idEquipa = i;
+    }
+
+    public void setAlcunha(String s) {
+        this.alcunha = s;
+    }
+
+    public String getImagePNG(){
+        return null;
+    }
+
+    public String toString(){
+        return (id + " | " + idTipo + " | " + idEquipa + " | " + alcunha);
+    }
+
+}
--- src/pt/ulusofona/lp2/crazyChess/Simulador.java
+++ src/pt/ulusofona/lp2/crazyChess/Simulador.java
@@ -2,28 +2,47 @@ package pt.ulusofona.lp2.crazyChess;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Simulador {
-    int tamanho;
-    List<CrazyPiece> pecas;
-    List<CrazyPiece> pecasA;
-    List<CrazyPiece> pecasB;
 
+    int tamanho;
+    ArrayList<CrazyPiece> pecas = new ArrayList<CrazyPiece>();
+    ArrayList<CrazyPiece> pecasA = new ArrayList<CrazyPiece>();
+    ArrayList<CrazyPiece> pecasB = new ArrayList<CrazyPiece>();
+    int turno;
 
    public Simulador(){
-    }
 
-    public boolean iniciaJogo(File ficheiroInicial){
+   }
+   public boolean iniciaJogo(File ficheiroInicial){
         try {
             Scanner scannerFicheiro = new Scanner(ficheiroInicial);
-
-            while (scannerFicheiro.hasNextLine()) {
+            tamanho = Integer.parseInt(scannerFicheiro.nextLine());
+            int pecasEmJogo = Integer.parseInt(scannerFicheiro.nextLine());
+            for (int i = 0; i < pecasEmJogo; i++) {
                 String linha = scannerFicheiro.nextLine();
-
+                String coluna[] = linha.split(":");
+                if (coluna.length == 4) {
+                    CrazyPiece peca = new CrazyPiece();
+                    peca.setId(Integer.parseInt(coluna[0]));
+                    peca.setIdTipo(Integer.parseInt(coluna[1]));
+                    peca.setIdEquipa(Integer.parseInt(coluna[2]));
+                    peca.setAlcunha(coluna[3]);
+                    pecas.add(peca);
+                    if(peca.idEquipa == 0) {
+                        pecasA.add(peca);
+                    } else {
+                        pecasB.add(peca);
+                    }
+                }
             }
-
+            for(int i = 0; i < tamanho; i++) {
+                String linha = scannerFicheiro.nextLine();
+            }
+            turno = 0;
         } catch (FileNotFoundException exception) {
             System.out.println("Erro: o ficheiro " + ficheiroInicial + " não foi encontrado.");
             return false;
@@ -32,25 +51,37 @@ public class Simulador {
     }
 
     public int getTamanhoTabuleiro(){
-        return 0;
+        return tamanho;
     }
 
     public boolean processaJogada(int xO, int yO, int xD, int yD){
-        return true;
+        if(xD == xO || xD == xO+1 || xD == xO - 1) {
+            if(yD == yO || yD == yO + 1 || yD == yO - 1) {
+                if (xD == xO && yD == yO) {
+                    return false;
+                } else {
+                    turno++;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-/*    public List<CrazyPiece> getPecasMalucas(){
-
+    public List<CrazyPiece> getPecasMalucas(){
+        return pecas;
     }
-*/
+
     public boolean jogoTerminado() {
-        return true;
+        return false;
     }
-
-  /*  public List<String> getAutores(){
-
+    public List<String> getAutores(){
+        ArrayList<String> autores = new ArrayList<String>();
+        autores.add("Augusto Gouveia - 21703303");
+        autores.add("Renato Cruz - 21705487");
+        return autores;
     }
-
+/*
     public List<String> getResultados(){
 
     }
@@ -59,8 +90,12 @@ public class Simulador {
         return 0;
     }
 
-    public int getIDEquipaAJogar(){
-        return 0;
+    public int getIDEquipaAJogar() {
+        if(turno % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
 }
